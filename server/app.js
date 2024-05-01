f211dea3fc78049e41cfe970599ed9523508a40d
Adithyan <adithyanraju60922@gmail.com>
2024-05-01 13:57:22 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -1,5 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 
-
 dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.static("./dist"));
+app.use("/api,", router);
+
+
+
+connect() 
+    .then(() => {
+        app.listen(process.env.PORT, error => {
+            if (error) return console.log(error);
+            console.log("server started!");
+        })
+    })
+    .catch(console.log)
